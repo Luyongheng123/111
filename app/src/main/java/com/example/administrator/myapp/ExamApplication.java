aae9d052c3ab18425698172132b26471461fb038
Luyongheng123 <1065357291@qq.com>
2017-07-05 11:22:28 +0800

    添加下载完毕在通知，使用cereiver更新界面

--- app/src/main/java/com/example/administrator/myapp/ExamApplication.java
+++ app/src/main/java/com/example/administrator/myapp/ExamApplication.java
@@ -1,13 +1,10 @@
 package com.example.administrator.myapp;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.example.administrator.myapp.bean.Exam;
-import com.example.administrator.myapp.bean.Examinfo;
-import com.example.administrator.myapp.bean.Result;
-import com.example.administrator.myapp.utils.OkHttpUtils;
-import com.example.administrator.myapp.utils.ResultUtils;
+import com.example.administrator.myapp.bean.ExamInfo;
+import com.example.administrator.myapp.biz.IExamBiz;
 
 import java.util.List;
 
@@ -16,82 +13,34 @@ import java.util.List;
  */
 
 public class ExamApplication extends Application{
-    Examinfo mExaminfo;
+    public static  String LOAD_EXAM_INFO="load_exam_info";
+    public static  String LOAD_EXAM_QUESTION="load_exam_question";
+    public static String LOAD_DATA_SUCCESS="load_exam_success";
+    ExamInfo mExamInfo;
     List<Exam> mExamList;
     static ExamApplication instance;
-
-
+IExamBiz biz;
 
     @Override
     public void onCreate() {
         super.onCreate();
         instance=this;
-        initData();
 
     }
-    public static ExamApplication getInstance() {
-        return instance;
+
+    public static ExamApplication getInstance() {return instance;}
+
+    public ExamInfo getExamInfo() {return mExamInfo;}
+
+    public void setExamInfo(ExamInfo examInfo) {
+        this.mExamInfo = examInfo;
     }
 
-    public Examinfo getmExaminfo() {
-        return mExaminfo;
-    }
-
-    public void setmExaminfo(Examinfo mExaminfo) {
-        this.mExaminfo = mExaminfo;
-    }
-
-    public List<Exam> getmExamlist() {
+    public List<Exam> getExamList() {
         return mExamList;
     }
 
-    public void setmExamlist(List<Exam> mExamlist) {
-        this.mExamList = mExamlist;
-    }
-
-    private void initData(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                OkHttpUtils<Examinfo> utils=new OkHttpUtils<>(instance);
-                String url="http://101.251.196.90:8080/JztkServer/examInfo";
-                utils.url(url)
-                        .targetClass(Examinfo.class)
-                        .execute(new OkHttpUtils.OnCompleteListener<Examinfo>(){
-                            @Override
-                            public void onSuccess(Examinfo result) {
-                                Log.e("main","result="+result);
-                                mExaminfo=result;
-                            }
-                            @Override
-                            public void onError(String error) {
-                                Log.e("main", "error= "+error );
-                            }
-                        });
-                OkHttpUtils<String> utils1=new OkHttpUtils<String>(instance);
-                String url2="http://101.251.196.90:8080/JztkServer/getQuestions?testType=rand";
-                utils1.url(url2)
-                        .targetClass(String.class)
-                        .execute(new OkHttpUtils.OnCompleteListener<String>(){
-                            @Override
-                            public void onSuccess(String jsonStr) {
-                            Result result= ResultUtils.getListResultFromJson(jsonStr);
-                     if (result!=null && result.getError_code()==0){
-                         List<Exam> list=result.getResult();
-                         if (list!=null&&list.size()>0){//已下载到数据
-                             mExamList=list;
-                         }
-                     }
-                            }
-
-                            @Override
-                            public void onError(String error) {
-                            Log.e("'main","error"+error);
-                            }
-                        });
-            }
-        }).start();
-
-
+    public void setExamList(List<Exam> examList) {
+        this.mExamList = examList;
     }
 }
--- app/src/main/java/com/example/administrator/myapp/activity/ExamActiviity.java
+++ app/src/main/java/com/example/administrator/myapp/activity/ExamActiviity.java
@@ -1,14 +1,21 @@
 package com.example.administrator.myapp.activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.administrator.myapp.ExamApplication;
 import com.example.administrator.myapp.R;
 import com.example.administrator.myapp.bean.Exam;
-import com.example.administrator.myapp.bean.Examinfo;
+import com.example.administrator.myapp.bean.ExamInfo;
+import com.example.administrator.myapp.biz.ExamBiz;
+import com.example.administrator.myapp.biz.IExamBiz;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -18,58 +25,118 @@ import java.util.List;
  */
 
 public class ExamActiviity extends AppCompatActivity {
-    TextView tvExamInfo,tvExmTitle,tv0p1,tv0p2,tv0p3,tv0p4;
+    TextView tvExamInfo, tvExmTitle, tv0p1, tv0p2, tv0p3, tv0p4;
     ImageView mImageView;
+    IExamBiz biz;
+    boolean isLoatExamInfo = false;
+    boolean isLoatQuestion = false;
+    LoadExamBroadcast mLoadExamBroadcast;
+    LoadQuestionBroadcast mLoadQuestionBroadcast;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam);
-       initView();
-        initData();
+        mLoadExamBroadcast=new LoadExamBroadcast();
+        mLoadQuestionBroadcast=new LoadQuestionBroadcast();
+        setListener();
+        initView();
+        loadData();
+    }
+private  void setListener(){
+    registerReceiver(mLoadExamBroadcast,new IntentFilter(ExamApplication.LOAD_EXAM_INFO));
+    registerReceiver(mLoadQuestionBroadcast,new IntentFilter(ExamApplication.LOAD_EXAM_QUESTION));
+}
+    private void loadData() {
+        biz = new ExamBiz();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                biz.beginExam();
+            }
+        }).start();
     }
 
-//    @Override
+    //    @Override
 //    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
 //        super.onCreate(savedInstanceState, persistentState);
 //    }
-    private void initView(){
+    private void initView() {
 
-        tvExamInfo= (TextView) findViewById(R.id.tv_examinfo);
-        tvExmTitle= (TextView) findViewById(R.id.tv_exam_title);
-        tv0p1= (TextView) findViewById(R.id.tv_op1);
-        tv0p2= (TextView) findViewById(R.id.tv_op2);
-        tv0p3= (TextView) findViewById(R.id.tv_op3);
-        tv0p4= (TextView) findViewById(R.id.tv_op4);
-         mImageView=(ImageView) findViewById(R.id.im_exam_image);
+        tvExamInfo = (TextView) findViewById(R.id.tv_examinfo);
+        tvExmTitle = (TextView) findViewById(R.id.tv_exam_title);
+        tv0p1 = (TextView) findViewById(R.id.tv_op1);
+        tv0p2 = (TextView) findViewById(R.id.tv_op2);
+        tv0p3 = (TextView) findViewById(R.id.tv_op3);
+        tv0p4 = (TextView) findViewById(R.id.tv_op4);
+        mImageView = (ImageView) findViewById(R.id.im_exam_image);
 
     }
-
-
 
     private void initData() {
-        Examinfo examinfo = ExamApplication.getInstance().getmExaminfo();
-        if (examinfo != null) {
-           showData(examinfo);
+        if (isLoatExamInfo && isLoatQuestion) {
+
+            ExamInfo examinfo = ExamApplication.getInstance().getExamInfo();
+            if (examinfo != null) {
+                showData(examinfo);
             }
-          List<Exam>examList=ExamApplication.getInstance().getmExamlist();
-        if (examList!=null){
-            showExam(examList);
-        }
-        }
-        private  void showExam(List<Exam> examList){
-           Exam exam=examList.get(0);
-            if (exam!=null){
-                tvExamInfo.setText(exam.getQuestion());
-                tv0p1.setText(exam.getItem1());
-                tv0p2.setText(exam.getItem2());
-                tv0p3.setText(exam.getItem3());
-                tv0p4.setText(exam.getItem4());
-                Picasso.with(ExamActiviity.this)
-                        .load(exam.getUrl())
-                        .into(mImageView);
+            List<Exam> examList = ExamApplication.getInstance().getExamList();
+            if (examList != null) {
+                showExam(examList);
             }
         }
-    private void showData (Examinfo examinfo){
+    }
+
+    private void showExam(List<Exam> examList) {
+        Exam exam = examList.get(0);
+        if (exam != null) {
+            tvExamInfo.setText(exam.getQuestion());
+            tv0p1.setText(exam.getItem1());
+            tv0p2.setText(exam.getItem2());
+            tv0p3.setText(exam.getItem3());
+            tv0p4.setText(exam.getItem4());
+            Picasso.with(ExamActiviity.this)
+                    .load(exam.getUrl())
+                    .into(mImageView);
+        }
+    }
+
+    private void showData(ExamInfo examinfo) {
         tvExamInfo.setText(examinfo.toString());
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mLoadExamBroadcast!=null){
+            unregisterReceiver(mLoadExamBroadcast);
+        }
+        if (mLoadQuestionBroadcast!=null){
+            unregisterReceiver(mLoadQuestionBroadcast);
+        }
     }
+
+    class LoadExamBroadcast extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            boolean isSuccess = intent.getBooleanExtra(ExamApplication.LOAD_DATA_SUCCESS, false);
+            Log.e("LoadExamBroadcast","LoadExamBroadcast.isSuccess"+isSuccess);
+            if (isSuccess) {
+                isLoatExamInfo = true;
+            }
+initData();
+        }
+    }
+
+    class LoadQuestionBroadcast extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            boolean isSuccess = intent.getBooleanExtra(ExamApplication.LOAD_DATA_SUCCESS, false);
+            Log.e("LoadQuestionBroadcast","LoadQuestionBroadcast.isSuccess"+isSuccess);
+            if (isSuccess) {
+                isLoatQuestion = true;
+            }
+
+        }
+    }
+}
